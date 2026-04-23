0421a8e425371bb2abf66ab356567192cad4c693
palomequega39-bit <palomeque.g.a.39@gmail.com>
2026-04-23 13:03:43 -0300

    Update app.py

--- app.py
+++ app.py
@@ -9,35 +9,39 @@ archivo = st.file_uploader("Subir archivo Excel o CSV", type=["csv", "xlsx"])
 
 if archivo:
 
+    # Cargar archivo
     if archivo.name.endswith(".csv"):
         df = pd.read_csv(archivo, sep=";", encoding="latin1")
     else:
         df = pd.read_excel(archivo)
 
+    # Limpiar nombres de columnas
     df.columns = df.columns.str.strip()
 
+    # Flags
     df["tiene_devolucion"] = df["Devolucion"].astype(str).str.upper() == "VERDADERO"
     df["tiene_foja"] = df["Foja"].notna()
     df["tiene_ci"] = df["CI"].astype(str).str.upper() == "VERDADERO"
 
+    # CABECERA (una fila por orden)
     cabecera = (
         df.groupby("Orden")
         .agg({
             "Apellido": "first",
             "Nombre": "first",
             "Dni": "first",
-            "Obra Social": "first",
-            "Fecha de CX": "first",
+            "ObraSocial": "first",
+            "FechaCX": "first",
             "Vendedor": "first",
             "Medico": "first",
-            "Medico Solicitante": "first",
+            "MedicoSolicitante": "first",
             "Foja": "first",
-            "Ci": "first",
+            "CI": "first",
             "Actividades": "first",
             "Institucion": "first",
             "Ciudad": "first",
             "Expediente": "first",
-            "Favorito": "first",
+            "Favotito": "first",
             "Devolucion": "first",
             "Prioridad": "first",
             "tiene_devolucion": "max",
@@ -47,24 +51,29 @@ if archivo:
         .reset_index()
     )
 
-    st.subheader("Órdenes")
+    st.subheader("Listado de Órdenes")
     st.dataframe(cabecera, use_container_width=True)
 
+    # Selección de orden
     orden = st.selectbox("Seleccionar Orden", cabecera["Orden"])
 
     cab = cabecera[cabecera["Orden"] == orden]
     det = df[df["Orden"] == orden]
 
-    st.subheader("Cabecera")
+    st.divider()
+
+    # CABECERA
+    st.subheader("Cabecera de la Orden")
     st.dataframe(cab, use_container_width=True)
 
-    st.subheader("Detalle")
+    # DETALLE
+    st.subheader("Detalle de Productos")
 
     columnas = [
-        "N° Remito",
-        "Fecha Remito",
+        "Remito",
+        "FechaR",
         "Producto",
-        "Cantidad",
+        "Q",
         "Lote",
         "Serie",
         "Vencimiento"
