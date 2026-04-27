2d6d0ef0cbf8c27aeffa5a107a4b1e1a02f90960
palomequega39-bit <palomeque.g.a.39@gmail.com>
2026-04-27 00:59:21 -0300

    Update app.js

--- app.js
+++ app.js
@@ -13,6 +13,11 @@ let seleccionados = new Set(); // Para guardar los IDs de las órdenes seleccion
 
 document.getElementById("buscadorGlobal")
   .addEventListener("input", aplicarFiltros);
+// Dentro de app.js, donde configures los eventos:
+document.getElementById("btnExportarPDF").onclick = () => {
+    // Llamamos a la función de export.js pasando las variables globales de app.js
+    exportarDetallePDF(ordenes, seleccionados);
+};
 
 /* =========================
    DROPZONE (NUEVO)
@@ -777,9 +782,4 @@ function handleCheck(event, ordenId) {
         document.getElementById("selectAll").checked = false;
     }
 }
-// Dentro de app.js, donde configures los eventos:
-document.getElementById("btnExportarPDF").onclick = () => {
-    // Llamamos a la función de export.js pasando las variables globales de app.js
-    exportarDetallePDF(ordenes, seleccionados);
-};
 
